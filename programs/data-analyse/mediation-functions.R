0e10b8f72cd7321d9b4b82acd5c4ff1413fe058e
shoganhennessy <senanhh@gmail.com>
2025-07-13 17:12:50 -0400

    Almost done, just needs boostrapping.

---
+++ programs/data-analyse/mediation-functions.R
@@ -0,0 +1,18 @@
+#!/usr/bin/R
+## Senan Hogan-Hennessy, 11 July 2025.
+## Relevant functions for mediation analysis, on data.
+
+
+################################################################################
+## Kitagawa (2015) test -> can this be done with Abadie weights?
+
+
+################################################################################
+## A Heckit function for mediation, returning FS + ATE + ADE + AIE.
+
+
+
+
+################################################################################
+## A semi-parametric  function for mediation, returning FS + ATE + ADE + AIE.
+
